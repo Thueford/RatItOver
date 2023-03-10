757af47de947088d2094f615ee5017242d8bcd4c
1d438ef6 <68151291+1d438ef6@users.noreply.github.com>
2023-03-11 00:10:53 +0100

    some smoll beautifull things

--- Assets/Scripts/Objects/BombenStuff.cs
+++ Assets/Scripts/Objects/BombenStuff.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Reflection;
 using UnityEngine;
 
 public class BombenStuff : MonoBehaviour
 {
+    public float delay_explosion;
+    public float explosion_strength;
+
     // Start is called before the first frame update
     void Start()
     {
