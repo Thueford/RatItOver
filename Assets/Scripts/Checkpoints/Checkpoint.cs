27dfb2ee3b8efd4e3565fc3b3ab3972dc431dfd6
tomsch43dlich <ts@ficonet.de>
2023-03-11 14:53:25 +0100

    Alphons mit Animation

--- Assets/Scripts/Checkpoints/Checkpoint.cs
+++ Assets/Scripts/Checkpoints/Checkpoint.cs
@@ -6,7 +6,12 @@ public class Checkpoint : MonoBehaviour
 {
     public int id;
     private bool activated = false;
+    Animation anim;
 
+    void Start()
+    {
+    }
+    
     public void activateCheckpoint() {
         activated = true;
     }
